186c2ecdfd94155134a4de9a340ceaa6c0b7b6fc
Andrew Burns <andrew@wyominghealthfairs.com>
2011-09-26 13:20:33 -0600

    Working check memory script

--- files/plugins/contrib/check_memory.rb
+++ files/plugins/contrib/check_memory.rb
@@ -4,12 +4,14 @@
 require 'rubygems'
 require 'nagios-probe'
 
+DEBUG=$DEBUG
 MAX_NUM=9999999
 class BaseProbe < Nagios::Probe	
+	alias :super_run :run
 	def run
 		decode_argvs
 		gather
-		super.run	
+		super_run
 	end
 	
 	def decode_argvs argv=nil
@@ -29,11 +31,35 @@ class BaseProbe < Nagios::Probe
 			raise "I don't know how to parse: $thres"
 		end
 	end
+	
+	def is_debug?
+		DEBUG
+	end
+	def debug msg
+		puts msg if is_debug?
+	end
+	
+	def round num,prec=0
+		return num.to_f.round if prec==0
+		prec_num = (10**prec).to_f
+		(num * prec_num).round() / prec_num
+	end
+	def to_pct num, total
+		round num/total.to_f*100, 2
+	end
 end
 
 class MyProbe < BaseProbe	
-	@critical_range = Range.new 0,0
-	@warning_range  = Range.new 0,0
+	attr_accessor :mem_free, :mem_used, :mem_total, :critical_range, :warning_range
+	
+	def initialize		
+		@critical_range = Range.new 0,0
+		@warning_range  = Range.new 0,0
+		@mem_free		= [0,0]
+		@mem_used		= [0,0]
+		@mem_total 	= 0
+	end
+	
 	def decode_argvs argv=nil
 		argv = ARGV unless argv
 		while argv.count > 0
@@ -49,25 +75,31 @@ class MyProbe < BaseProbe
 		end
 	end
 	
-	@mem_free		= [0,0]
-	@mem_used		= [0,0]
-	@mem_total 	= 0
-	def gather
-		puts "Gathering..."
-		mem = %x{free -tm}
-		match = mem.match /^Total:\s+(\d+)\s+(\d+)\s+(\d+)/
-		raise "couldn't match!" unless match
- 		puts match
-		puts @mem_free[0]
+	
+	def gather		
+		# @mem_free		= [0,0]
+		# @mem_used		= [0,0]
+		# @mem_total 	= 0
+		mem = %x{free -tm} unless is_debug?
+		mem = %Q{
+             total       used       free     shared    buffers     cached
+Mem:          8001        448       7553          0         51        143
+-/+ buffers/cache:        253       7748
+Swap:        17099          0      17099
+Total:       25101        448      24653
+} if is_debug?
+		match = mem.match /Mem:\s+(\d+)\s+(\d+)\s+(\d+)/
+		raise "couldn't match!" unless match && match.length==4
+ 		debug "MATCH! t=#{match[1]} u=#{match[2]} f=#{match[3]}"
 		@mem_free[0] = match[1].to_i
-		#@mem_used[0] = match[2].to_i
-		#@mem_total   = match[3].to_i 
+		@mem_used[0] = match[2].to_i
+		@mem_total   = match[3].to_i 
 		
-		#@mem_free[1] = @mem_free[0] / @mem_total
-		# @mem_used[1] = @mem_used[0] / @mem_total
-		puts "Total: 	#{@mem_total}MB"
-		# puts " Free: 	#{@mem_free[0]}MB/#{@mem_free[1]}%"
-		# puts " Used: 	#{@mem_used[0]}MB/#{@mem_used[1]}%"
+		@mem_free[1] = to_pct @mem_free[0], @mem_total
+		@mem_used[1] = to_pct @mem_used[0], @mem_total
+		debug "Total: 	#{@mem_total} MB"
+		debug " Free: 	#{@mem_free[0]} MB/#{@mem_free[1]}%"
+		debug " Used: 	#{@mem_used[0]} MB/#{@mem_used[1]}%"
 	end
 	
   def check_crit
@@ -90,18 +122,19 @@ class MyProbe < BaseProbe
   end
 
   def ok_message
-    "Memory usage optimal"
+    "Used #{@mem_used[0]} MB/#{@mem_used[1]}% out of #{@mem_total} MB"
   end
 end
 
-begin
-  options = {} # Nagios::Probe constructor accepts a single optional param that is assigned to @opts
-  probe = MyProbe.new(options)
-  probe.run
-rescue Exception => e
-  puts "Unknown: " + e
-  exit Nagios::UNKNOWN
-end
+if __FILE__ == $0:
+	begin
+	  probe = MyProbe.new
+	  probe.run
+	rescue Exception => e
+	  puts "Unknown: " + e
+	  exit Nagios::UNKNOWN
+	end
 
-puts probe.message
-exit probe.retval
+	puts probe.message
+	exit probe.retval
+end
